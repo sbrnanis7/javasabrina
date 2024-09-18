67b48526cc22c7e3f8b9d037133f131ba146b7f4
sbrnanis7 <sabrinazayadi0621@gmail.com>
2024-09-18 13:15:10 +0800

    example 2

---
+++ controllers/DifferentScope.java
@@ -0,0 +1,22 @@
+public class DifferentScope {
+
+    int instanceVariable = 10;
+    double instanceVariableDouble = 10.8;
+    String instanceVariableString = "This is string";
+
+    static int instanceVariableStatic = 20;
+
+    public void showLocalvariable() {
+        String localStringVariable = "Finish!";
+        int localVariable = 30;
+        System.out.println("My local variable is:" + localVariable + " " + localStringVariable);
+    }
+    
+    public static void main(String[] args){
+        DifferentScope example = new DifferentScope();
+        example.showLocalvariable();
+        System.out.println("My local variable is: " + example.instanceVariable + " " +example.instanceVariableString);
+
+        
+    }
+}
